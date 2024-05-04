9fbd81e48eb576bc47ac76c48263856fe29f7892
ubeyid oumer <ubeyidah@gmail.com>
2024-05-04 16:00:47 +0300

    delivery info

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions =  [
+  {
+    deliveryId: "1",
+    priceCents: 0,
+    deliveryDay: 9
+  },
+  {
+    deliveryId: "2",
+    priceCents: 499,
+    deliveryDay: 3
+  },
+  {
+    deliveryId: "3",
+    priceCents: 999,
+    deliveryDay: 1
+  }
+]
