3d1819629f5de751d345bdaa4bb9fc0e0fd5c823
ubeyid oumer <ubeyidah@gmail.com>
2024-05-10 15:26:45 +0300

    remove test datas in the cart

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,4 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 1,
-  deliveryOptionId: "2"
-},
-{
-  id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 3,
-  deliveryOptionId: "3"
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export const saveToStorage = () => localStorage.setItem("cart", JSON.stringify(cart));
 export const getMatchCartItem = (id) => cart.filter(item => item.id === id);
