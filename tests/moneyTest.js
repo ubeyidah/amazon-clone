d540c4e68ba49ca263e8e079ab2314e415935c23
ubeyid oumer <ubeyidah@gmail.com>
2024-05-10 15:25:49 +0300

    remove default test and add money test

---
+++ tests/moneyTest.js
@@ -0,0 +1,20 @@
+import formatCurrency from "../scripts/utils/formatCurrency.js";
+
+
+describe("test suite: formatCurrency", () => {
+
+  it("convert cents to dollars" , () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("works with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00")
+  })
+
+ describe("rounding" , () => {
+  it("round up to the nearest cents" , () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+    expect(formatCurrency(2000.6)).toEqual("20.01");
+  })
+ })
+});
